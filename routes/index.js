11a3b9fd5d6398e0508892f374c357fb336f09f2
Allia14 <allialilac14@gmail.com>
2023-12-16 12:02:02 +0700

    penjelasan

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+//sintaks diatas berfungsi untuk mengimpor modul Express ke dalam kode ini.
+
 const router = express.Router()
+/**
+ * 1. Menciptakan sebuah instance baru dari Express Router
+ * 2. Memisahkan logika routing dari aplikasi utama
+ * 3. Pemasangan middleware dan route handlers
+ * 4. Mounting ke aplikasi Express utama
+ */
+
 const produkRouter = require('./produk.route')
 const produsenRouter = require('./produsen.route')
 
